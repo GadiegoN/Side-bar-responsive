640d0c05725b306f6718b80a11b48af145c90c77
GadiegoN <ngadiego@gmail.com>
2021-09-11 18:56:52 -0300

    :package: Adicionado funçao de busca no menu

--- pages/menu/script.js
+++ pages/menu/script.js
@@ -9,3 +9,20 @@ btn.onclick = function() {
 searchBtn.onclick = function() {
     sidebar.classList.toggle("active");
 }
+
+function shearchTitle() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("nav_ul");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
